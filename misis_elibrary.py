5cce9b26c00b463819e7ca8553bfb9e7eb626cf1
Layerex <layerex@dismail.de>
2022-12-02 22:18:20 +0300

    Bump version

--- misis_elibrary.py
+++ misis_elibrary.py
@@ -2,7 +2,7 @@
 
 __prog__ = "misis-elibrary"
 __desc__ = "Программа для загрузки книг из электронной библиотеки МИСИС."
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 import argparse
 import imghdr
